d34ba9da94e70b9520f3a6f9df35dd9b79718981
Bryan Chong <yih00@graduate.utm.my>
2023-08-21 16:18:57 +0800

    set up

---
+++ trial.py
@@ -0,0 +1,7 @@
+from qutip import *
+import numpy as np
+import matplotlib.pyplot as plt
+from Hamiltonian import Hamiltonian
+
+psi01 = tensor(fock(2,1),fock(2,0))
+psi02 = tensor(fock(2,0),fock(2,0))
